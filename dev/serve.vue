b0072e05c6ed0bb926efb8978dd12e96b32b207a
Horia Rotaru <horia.rotaru1@gmail.com>
2021-08-07 09:52:01 +0100

    Init

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueHeadlessUiSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  VueHeadlessUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-headless-ui-sample />
+  </div>
+</template>
