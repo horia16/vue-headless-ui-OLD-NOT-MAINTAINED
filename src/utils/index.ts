ad7337f7096b78dc17fdabdb18939c3951c9685b
Horia Rotaru <horia.rotaru1@gmail.com>
2021-12-10 12:30:42 +0000

    Removed old files.

--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as randomString } from "./random";
-export { default as isMissingInjectable } from "./is-missing-injectable";
-export { default as injectionKeys } from "./injection-keys/index";
